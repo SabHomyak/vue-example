83efa0817cf09dea7c8f35d431393d0395c43876
Vadim Bobylov <davyjonessss@ukr.net>
2021-09-06 06:41:13 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="app">
-        <post-form
-                @create="createPost"
-        />
+        <h1>Страница с постами</h1>
+        <my-button style="margin:15px 0" @click="showDialog">Создать пост</my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
+                    @create="createPost"
+            />
+        </my-dialog>
         <post-list
                 :posts="posts"
+                @remove="removePost"
         />
     </div>
 </template>
@@ -12,10 +17,13 @@
 <script>
     import PostList from "./components/PostList";
     import PostForm from "./components/PostForm";
+    import MyDialog from "./components/UI/MyDialog";
+    import MyButton from "./components/UI/MyButton";
 
     export default {
         components: {
-            PostList, PostForm
+            MyButton,
+            MyDialog, PostList, PostForm
         },
         data() {
             return {
@@ -24,12 +32,20 @@
                     {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
                     {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
                     {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-                ]
+                ],
+                dialogVisible: false
             }
         },
         methods: {
             createPost(post) {
                 this.posts.push(post);
+                this.dialogVisible = false;
+            },
+            removePost(post) {
+                this.posts = this.posts.filter(filterPost => filterPost !== post)
+            },
+            showDialog() {
+                this.dialogVisible = true
             }
         }
     }
